c2b32cf72d1ee9e9dab1578ab9075b0554833124
Juho Pousi <juho.pousi@aalto.fi>
2021-12-28 17:59:11 +0200

    change websocket to use https

--- rps-frontend/src/utils/constants.ts
+++ rps-frontend/src/utils/constants.ts
@@ -1,2 +1,2 @@
-export const webSocketAddress = 'ws://bad-api-assignment.reaktor.com/rps/live';
+export const webSocketAddress = 'wss://bad-api-assignment.reaktor.com/rps/live';
 export const apiPrefix = process.env.NODE_ENV === 'production' ? '' : 'http://localhost:3000/';
