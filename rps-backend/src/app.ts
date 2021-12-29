793d4af1f85d942de321c2acb6a9e8375a5e6e99
Juho Pousi <juho.pousi@aalto.fi>
2021-12-29 14:55:58 +0200

    fix one typo and run data fetch also immediately when app is started

--- rps-backend/src/app.ts
+++ rps-backend/src/app.ts
@@ -22,25 +22,28 @@ app.use(express.json());
 /**
  * Keep BadApi and DB in synch by getting new game results frequently (on every ten minute)
  */
-let lock = false;
-setInterval(() => {
-  if (!lock) {
-    lock = true;
-    console.log('syncing data');
-    syncBadApiAndDb()
-      .then((result) => {
-        console.log(result);
-        lock = false;
-      })
-      .catch((e) => {
-        console.log(e);
-        lock = false;
-      });
-  }
-  else {
-    console.log('lock is set');
-  }
-}, 600000);
+ let lock = false;
+ const syncJob = () => {
+   if (!lock) {
+     lock = true;
+     console.log('syncing data');
+     syncBadApiAndDb()
+       .then((result) => {
+         console.log(result);
+         lock = false;
+       })
+       .catch((e) => {
+         console.log(e);
+         lock = false;
+       });
+   }
+   else {
+     console.log('lock is set');
+   }
+ }
+ 
+ syncJob(); // run also immediately when app is started
+ setInterval(syncJob, 600000);
 
 app.get('/ping', (_req, res) => {
   res.send('This is RPS result api!');
